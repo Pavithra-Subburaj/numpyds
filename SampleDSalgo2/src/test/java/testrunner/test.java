11e54751edc6d17526baed86f35c111919e03173
unknown <auxilia06@gmail.com>
2024-10-24 12:14:59 -0700

    Florence Commit

--- SampleDSalgo2/src/test/java/testrunner/test.java
+++ SampleDSalgo2/src/test/java/testrunner/test.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		
-		features = { "src/test/resources/feature1"},//location of feature files
+		features = { "src/test/resources/feature1/stack.feature"},//location of feature files
 		glue={"ds_stepdefinition"} ,//location of step definition
 		plugin= {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 		)
